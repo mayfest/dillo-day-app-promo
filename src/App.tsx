d3d855b26c84a6af6ef4cfa82d267c77831a4091
Ethan Pineda <joshuapineda66@gmail.com>
2025-05-04 16:01:06 -0500

    fixing issues to test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react';
-
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className='text-red-500 text-6xl'>hello world</h1>
