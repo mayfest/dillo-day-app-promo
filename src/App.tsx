f7ac97a1a4ed4b0208d23b217b5e20c8d02ede61
Ethan Pineda <joshuapineda66@gmail.com>
2025-05-04 15:59:30 -0500

    adding prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-red-500 text-6xl">hello world</h1>
+      <h1 className='text-red-500 text-6xl'>hello world</h1>
     </>
   );
 }
